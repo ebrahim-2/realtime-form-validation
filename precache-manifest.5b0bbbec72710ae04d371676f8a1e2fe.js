2373f4ca4100d84c51a55d9ca75e08e25276822f
EbrahimKreem <ebrahemkreem40@gmail.com>
2019-11-08 16:35:37 +0300

    Updates

---
+++ precache-manifest.5b0bbbec72710ae04d371676f8a1e2fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7a41bd8588ea3eb05fe01117196012",
+    "url": "/realtime-form-validation/index.html"
+  },
+  {
+    "revision": "e78b439dbcfd9ee1a963",
+    "url": "/realtime-form-validation/static/css/main.6baeb1ac.chunk.css"
+  },
+  {
+    "revision": "8e3c3b80b16d678e82e1",
+    "url": "/realtime-form-validation/static/js/2.97ef082a.chunk.js"
+  },
+  {
+    "revision": "e78b439dbcfd9ee1a963",
+    "url": "/realtime-form-validation/static/js/main.eb288a27.chunk.js"
+  },
+  {
+    "revision": "ff532b9cb0df15c12b52",
+    "url": "/realtime-form-validation/static/js/runtime~main.40ee1230.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/realtime-form-validation/precache-manifest.5b0bbbec72710ae04d371676f8a1e2fe.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/realtime-form-validation/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
